90964da069b55fbc2ddbd3372f636443c7d9963e
Daisy Collins <daisydcollins12@gmail.com>
2021-07-17 20:54:26 -0700

    im giving up

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-function Pizza(size, crustArray, meatArray, vegArray) {
+function Pizza(size, meatArray, vegArray, crustArray) {
   this.size = size;
-  this.crust = crustArray;
   this.meatArray = meatArray;
   this.vegArray = vegArray;
+  this.crust = crustArray;
   this.ongoingPrice = 0;
 }
 
@@ -20,13 +20,42 @@ Pizza.prototype.priceOfSize = function() {
 } 
 
 Pizza.prototype.priceOfMeat = function() {
-  return this.ongoingPrice += (this.meatArray.length + 3);
+  return this.ongoingPrice + 3;
 }
 
 Pizza.prototype.priceOfVeg = function() {
-  return this.ongoingPrice += (this.vegArray.length + 1.5);
+  return this.ongoingPrice + 1.5;
 }
 
-Pizza.prototype.priceOfCrust = function {
-  return this.ongoingPrice += (this.crustArray.length + 2);
+Pizza.prototype.priceOfCrust = function() {
+  return this.ongoingPrice + 2;
 }
+
+$(document).ready(function() {
+  $("form#menuItems").submit(function(event) {
+    event.preventDefault();
+    let size = $("#size").val();
+    let meatArray = [];
+    let vegArray = [];
+    let crustArray = [];
+    $("input:checkbox[name=meat]:checked").each(function() {
+      meatArray.push($(this).val());
+    });
+    $("input:checkbox[name=veggie]:checked").each(function() {
+      vegArray.push($(this).val());
+    });
+    $("input:checkbox[name=crust]:checked").each(function() {
+      crustArray.push($(this).val());
+    });
+
+    let newOrder = new Pizza(size,meatArray,vegArray,crustArray)
+
+    newOrder.priceOfSize();
+    newOrder.priceOfMeat();
+    newOrder.priceOfVeg();
+    newOrder.priceOfCrust();
+    const totalPrice = newOrder.ongoingPrice;
+    $("#total").show();
+    $("#damage").text(totalPrice)
+  })
+})
